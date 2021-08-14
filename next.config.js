8ffedfab1dc3eb98d8b760dc1f35a95fe7735a91
trungphan163 <trung.phan@usumsoftware.com>
2021-08-14 21:31:18 +0700

    init nextjs template

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+    reactStrictMode: true,
+    webpack5: false,
+    webpack: (config, { isServer }) => {
+        // Fixes packages that depend on fs/module module
+        if (!isServer) {
+            config.node = { fs: "empty", module: "empty" };
+        }
+        return config;
+    },
+};
